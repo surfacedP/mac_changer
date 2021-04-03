f349f3d35edbe8e6f235fc66c443ea5e234e1032
user <patrick.brook@outlook.com>
2021-04-03 20:16:24 +0100

    Black reformat

--- mac_changer.py
+++ mac_changer.py
@@ -6,8 +6,18 @@ import re
 
 
 @click.command()
-@click.option('-i', '--interface', prompt='Input Interface:', help="Interface to change MAC address for")
-@click.option('-m', '--mac', prompt='Input MAC address:', help="MAC address to change to for Interface")
+@click.option(
+    "-i",
+    "--interface",
+    prompt="Input Interface:",
+    help="Interface to change MAC address for",
+)
+@click.option(
+    "-m",
+    "--mac",
+    prompt="Input MAC address:",
+    help="MAC address to change to for Interface",
+)
 def begin(interface, mac):
     get_mac(interface)
     change_mac(interface, mac)
@@ -15,7 +25,9 @@ def begin(interface, mac):
 
 def get_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    mac_address_search_result = re.search(
+        r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result)
+    )
 
     if mac_address_search_result:
         print(mac_address_search_result.group(0))
@@ -30,5 +42,5 @@ def change_mac(interface, new_mac):
     get_mac(interface)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     begin()
