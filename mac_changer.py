faa61cd6df24f1bbcb2dfa2315af40ef52686552
user <patrick.brook@outlook.com>
2021-04-03 20:14:00 +0100

    Cleanup

--- mac_changer.py
+++ mac_changer.py
@@ -6,25 +6,29 @@ import re
 
 
 @click.command()
-@click.option('-i', '--interface', prompt='Interface')
-@click.option('-m', '--mac', prompt='MAC address')
-def current_mac(interface, mac):
+@click.option('-i', '--interface', prompt='Input Interface:', help="Interface to change MAC address for")
+@click.option('-m', '--mac', prompt='Input MAC address:', help="MAC address to change to for Interface")
+def begin(interface, mac):
+    get_mac(interface)
+    change_mac(interface, mac)
+
+
+def get_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
 
     if mac_address_search_result:
         print(mac_address_search_result.group(0))
-        change_mac(interface, mac)
     else:
         print("Could not read MAC")
 
 
 def change_mac(interface, new_mac):
-    print("Changing MAC for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
+    get_mac(interface)
 
 
 if __name__ == '__main__':
-    current_mac()
+    begin()
